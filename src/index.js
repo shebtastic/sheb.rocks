fa619a7876e2ad5cbe9c0bdbb2b41aef7de959c8
Sheb <shebtastic@googlemail.com>
2019-03-10 17:40:13 +0100

    added sw.js registration, commented out preact parts and registered listener to handle vh alternative

--- src/index.js
+++ src/index.js
@@ -4,28 +4,35 @@ import './style'
 import Header from './components/header'
 import Pages from './components/pages'
 
-render(
-  <Header />,
-  document.querySelector('body'),
-  document.body.insertBefore(
-    document.createElement('header'),
-    document.querySelector('main')
-  )
-)
+const setWindowInnerHeightStyle = () => {
+  document.documentElement.style.setProperty('--window-inner-height', `${window.innerHeight}px`);
+}
 
-render(
-  <Pages />,
-  document.querySelector('main'),
-  document.body.insertBefore(
-    (() => {
-      let div = document.createElement('div')
-      div.id = 'app'
-      return div
-    })(),
-    document.querySelector('main > div#app')
-  )
-)
+document.addEventListener('resize', setWindowInnerHeightStyle)
+setWindowInnerHeightStyle()
 
+// render(
+//   <Header />,
+//   document.querySelector('body'),
+//   document.body.insertBefore(
+//     document.createElement('header'),
+//     document.querySelector('main')
+//   )
+// )
+//
+// render(
+//   <Pages />,
+//   document.querySelector('main'),
+//   document.body.insertBefore(
+//     (() => {
+//       let div = document.createElement('div')
+//       div.id = 'app'
+//       return div
+//     })(),
+//     document.querySelector('main > div#app')
+//   )
+// )
+//
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
 }
