eeac8409cdbc252163e52ac9ae6825e90a979115
Sheb <shebtastic@googlemail.com>
2019-03-11 01:27:08 +0100

    added log to registered sw

--- src/webApp/index.js
+++ src/webApp/index.js
@@ -28,5 +28,6 @@ import Pages from './pages'
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
+    .then(() => console.log('Service Worker registered! Hey! 👋'))
 }
 
