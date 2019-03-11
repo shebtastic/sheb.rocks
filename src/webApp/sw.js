46a0b63ad49ebbf17140be20c55876a8f6aaa705
Sheb <shebtastic@googlemail.com>
2019-03-11 01:34:10 +0100

    added initial cache to sw

--- src/webApp/sw.js
+++ src/webApp/sw.js
@@ -1,8 +1,30 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js')
 
 if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
+  console.log(`Yay! Workbox is loaded 🎉`)
+
+  workbox.routing.registerRoute(
+    /\.css$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'css-cache',
+    })
+  )
+
+  workbox.routing.registerRoute(
+    /\.(?:png|jpg|jpeg|svg|gif)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'image-cache',
+    })
+  )
+
+  workbox.routing.registerRoute(
+    /\.woff2$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'font-cache',
+    })
+  )
+
 } else {
-  console.log(`Boo! Workbox didn't load 😬`);
+  console.log(`Boo! Workbox didn't load 😬`)
 }
 
